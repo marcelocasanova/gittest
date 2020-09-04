80ffaa12328d9737c7a1fc3e262830102805b470
marcelocasanova <42650657+marcelocasanova@users.noreply.github.com>
2020-09-03 19:48:18 -0500

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int time=20;
+    string ftime = (time<10)? "Good Morning" : ((time<20)? "Good Day" : "Good Evening"); 
+    cout<<ftime;
+    return 0;
+}
